e5d21c17fc4a24daad966fc2d10ad968b09d4f9b
wesbro <wrbrown12@gmail.com>
2022-03-04 22:30:46 -0500

    first commit. created file

---
+++ server.js
@@ -0,0 +1,32 @@
+////DEPENDENCIES////
+
+const { append } = require("express/lib/response");
+
+///DATABASE CONNECTION////
+
+
+///MODELS///
+
+
+///APP OBJECT SETUP///
+
+
+
+///MIDDLEWARE///
+
+///ROUTES///
+
+//INDEX
+
+//NEW
+
+//UPDATE
+
+//CREATE
+
+//EDIT
+
+//SHOW
+
+
+app.listen(Port, () => console.log(`Live form ${PORT}`))
